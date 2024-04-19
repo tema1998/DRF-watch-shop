0a5385fdd3cfda176ad47167b990addb94ebed4c
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-04-19 21:01:58 +0300

    Add JWT settings

--- api/core/views.py
+++ api/core/views.py
@@ -25,7 +25,7 @@ class RegisterView(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = RegisterSerializer
 
-    def post(self,request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
