ac5df784b5c35fe52309772a97e90b94c659a16d
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-04-26 09:57:56 +0300

    Add dockstring to news models Add verbose names to news models

--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,15 @@ from unidecode import unidecode
 
 
 class News(models.Model):
-    slug = models.SlugField(unique=True)
-    title = models.CharField(max_length=200)
-    text = RichTextUploadingField()
-    image = models.ImageField()
-    created_at = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    """
+    Model for storing news.
+    """
+    slug = models.SlugField(unique=True, verbose_name="Slug")
+    title = models.CharField(max_length=200, verbose_name="Title")
+    text = RichTextUploadingField(verbose_name="News text")
+    image = models.ImageField(verbose_name="Image")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Date of creation")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Author")
     likes = models.ManyToManyField(User, blank=True, related_name="news_like", verbose_name='Likes')
 
     def __str__(self):
@@ -29,7 +32,13 @@ class News(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Model for storing comments to news.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user', verbose_name='User')
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comment_news', verbose_name='News')
     text = models.TextField(verbose_name='Comment text')
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Date of creation")
+
+    def __str__(self):
+        return f'{self.created_at} - {self.user}'
