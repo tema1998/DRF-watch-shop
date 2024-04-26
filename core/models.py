0b259a0670d6d01c9bd13f129cd3e058402ab883
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-04-26 10:27:34 +0300

    Add dockstring to core models Add verbose names to core models

--- core/models.py
+++ core/models.py
@@ -9,31 +9,43 @@ from unidecode import unidecode
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=200)
+    """
+    Model for storing manufacturer country.
+    """
+
+    name = models.CharField(max_length=200, verbose_name="Country")
 
     def __str__(self):
         return self.name
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=200)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    description = RichTextUploadingField(blank=True)
+    """
+    Model for storing brand.
+    """
+
+    name = models.CharField(max_length=200, verbose_name="Brand")
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name="Country")
+    description = RichTextUploadingField(blank=True, verbose_name="Description")
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    model = models.CharField(max_length=200)
-    slug = models.SlugField(blank=True, unique=True)
-    description = RichTextUploadingField()
-    image = models.ImageField()
-    created_at = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    discount = models.IntegerField(default=0)
+    """
+    Model for storing product.
+    """
+
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name="Brand")
+    model = models.CharField(max_length=200, verbose_name="Model")
+    slug = models.SlugField(blank=True, unique=True, verbose_name="Slug")
+    description = RichTextUploadingField(verbose_name="Description")
+    image = models.ImageField(verbose_name="Image")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Date of creation")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Author")
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Price, $")
+    discount = models.IntegerField(default=0, verbose_name="Discount, %")
 
     def __str__(self):
         return self.model
@@ -48,13 +60,21 @@ class Product(models.Model):
 
 
 class Feedback(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(blank=True)
-    review = models.CharField(max_length=1000)
-    created_at = models.DateTimeField(default=timezone.now)
+    """
+    Model for storing feedback.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="User")
+    image = models.ImageField(blank=True, verbose_name="Image")
+    review = models.CharField(max_length=1000, verbose_name="Review")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Date of creation")
 
 
 class Reviews(models.Model):
+    """
+    Model for storing reviews to watches.
+    """
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
     text = models.TextField()
