45d64fa68e5b167dadea9cbb0e5ef94e48b64971
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-05-02 19:55:05 +0300

    Add all models to admin of Core app.

--- core/admin.py
+++ core/admin.py
@@ -1,19 +1,43 @@
 from django.contrib import admin
-from .models import Product, Country, Brand
+
+from .models import Product, Country, Brand, PaymentProcess, Order, OrderProduct, Feedback, Reviews
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Country, CountryAdmin)
-admin.site.register(Brand, BrandAdmin)
+@admin.register(PaymentProcess)
+class PaymentProcessAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    pass
