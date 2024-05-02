b97967c658ac4b65776c562505b9295d261adea3
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-05-02 19:56:17 +0300

    Add all models to admin of Blog app.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import News, Comment
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
