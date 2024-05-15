7154b75180a5bae22e8d41db122b13dc94937bdc
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-05-15 10:42:46 +0300

    Frontend. Fix discount display on the cart page.

--- frontend/pages/cart.vue
+++ frontend/pages/cart.vue
@@ -40,7 +40,13 @@
             </div>
 
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white">${{ product.product.price }} </span>
+                <div v-if="product.product.discount === 0">
+                  <span class="text-3xl font-bold text-gray-900 dark:text-white">${{ product.product.price }} </span>
+                </div>
+                <div v-else>
+                  <span class="line-through text-3xl font-bold text-gray-900 dark:text-white">${{ product.product.price }} </span>
+                  <span class="text-3xl font-bold text-red-400 dark:text-white">${{ product.product.discount }} </span>
+                </div>
 
                 <div class="flex">
                   <a @click.prevent="addToCart(product_id=`${product.product.id}`, product_price=`${product.product.price}`)" href="#" class="mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</a>
